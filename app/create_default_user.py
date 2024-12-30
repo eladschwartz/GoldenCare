7f2781f867ad85da895eb8753522efd66b312532
Elad Shwartz <eladshw@gmail.com>
2024-12-30 13:59:14 +0200

    fixed a bug

--- app/create_default_user.py
+++ app/create_default_user.py
@@ -1,15 +1,17 @@
-from app.models import User 
+from app.models import User
 from app.database import get_db
 from app.utils import hash
 from app.config import settings
 
+
 def create_default_user():
-    db = get_db()
-    admin_user = db.query(User).filter(
+    db = next(get_db())
+    
+    try:
+        admin_user = db.query(User).filter(
             User.email == settings.admin_email
-        ).first()
-    if not admin_user:
-            # Create new admin user
+            ).first()
+        if not admin_user:
             admin_user = User(
                 name=settings.admin_name,
                 department_id = 1,
@@ -19,8 +21,10 @@ def create_default_user():
             db.add(admin_user)
             db.commit()
             print("Default admin user created successfully!")
-    else:
+        else:
             print("Admin user already exists!")
+    finally:
+        db.close()
  
         
 
