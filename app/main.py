e9bf9169b1e5a66e8a31d2ead0e33f4ac3827108
Elad Shwartz <eladshw@gmail.com>
2024-12-31 19:59:21 +0200

    added a rate limiter

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ from .routers.routers import ROUTERS
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from .config import settings
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 app = FastAPI()
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["30/minute"])
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
